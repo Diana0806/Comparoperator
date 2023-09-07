9e023b92797637519be79d2bb35c222a5793a88a
HHOZKAN <ozkn.haci@gmail.com>
2023-09-07 15:52:24 +0200

    navbar + accueil + footer

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include('./utilscss/navbar.php'); ?>
+
+
+<div class='container text-center text-index'>
+
+    <h1>Comparoperator</h1>
+    <p>" Embarquez pour une aventure sans limites, explorez des horizons lointains et créez des souvenirs qui illumineront votre vie pour toujours. Le monde vous attend, prêt à vous dévoiler ses merveilles."</p>
+    <button type="button" class="btn btn-transparent border-light">En savoir plus -> </button>
+
+</div>
+
+
+
+<?php include('./utilscss/footer.php'); ?>
